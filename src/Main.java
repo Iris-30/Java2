fd7a72e1f6a15ff46edd573fa4a7d6da6aa55889
IRINA <aksenemko@icloud.com>
2024-11-12 18:33:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 10000;
+        if(x > 20) {
+            System.out.println("вам начислен бонус в размере");
+        }
+        int y = 20;
+
+        int bonus = x / y ;
+        System.out.println(bonus);
+        System.out.println("миль");
+
+    }
+}
